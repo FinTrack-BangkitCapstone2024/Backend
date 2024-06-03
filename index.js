81d2f3226f9c2abf1909e00204e069daa1ab1341
Papazy <fajryjobs@gmail.com>
2024-06-03 23:55:11 +0700

    add docs

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ const app = express()
 const cors = require('cors');
 const routes = require('./routes/index');
 const dotenv = require('dotenv');
+const path = require('path');
 dotenv.config();
 
 const port = process.env.PORT ;
 
 app.use(cors({origin: true}));
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', routes);
+
+app.get('/', (req, res) => {
+  res.redirect('/docs');
+});
+app.get('/docs', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'docs', 'index.html'));
+});
+
 
 app.listen(port, () => {
   console.log(`app running at http://localhost:${port}`)
