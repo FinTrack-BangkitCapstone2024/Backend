d6a2eb0d30dcfe6cf1210823149dc6a46344ec05
jenifer <jvr88051@gmail.com>
2024-06-16 02:47:01 +0700

    feat: validator & add image

--- controllers/inventaris_controller.js
+++ controllers/inventaris_controller.js
@@ -3,8 +3,9 @@ const Inventaris = require('../models/inventaris_model');
 const { db } = require('../config/firebase');
 const bucket = require('../config/gcs');
 
-const inventaris_controller = {
+const validator = require('../routes/validator');
 
+const inventaris_controller = {
   getAllInventaris: async (req, res) => {
     try {
       const items = await Inventaris.getAll();
@@ -27,16 +28,14 @@ const inventaris_controller = {
     }
   },
   addInventaris: async (req, res) => {
-    const {
-      user_id, item_name, item_type, quantity, unit, description,
-    } = req.body;
     const { file } = req;
 
     if (!file) {
-      return res.status(400).send('No file uploaded.');
+      return res.status(400).send('No image uploaded.');
     }
 
-    const blob = bucket.file(file.originalname);
+    const folderName = 'inventaris';
+    const blob = bucket.file(`${folderName}/${req.file.originalname}`);
     const blobStream = blob.createWriteStream({
       resumable: false,
       contentType: file.mimetype,
@@ -45,21 +44,20 @@ const inventaris_controller = {
     blobStream.on('error', (err) => res.status(500).send({ message: err.message }));
 
     blobStream.on('finish', async () => {
-      const photo_url = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+      const photo_url = `https://storage.googleapis.com/${bucket.name}/${folderName}/${blob.name}`;
 
-      const detailItem = {
-        user_id,
-        photo_url,
-        item_name,
-        item_type,
-        quantity,
-        unit,
-        description,
-
+      const Items = {
+        user_id: req.body.user_id,
+        photo_url: photo_url,
+        item_name: req.body.item_name,
+        item_type: req.body.item_type,
+        quantity: req.body.quantity,
+        unit: req.body.unit,
+        description: req.body.description,
       };
 
       try {
-        const inventaris = await Inventaris.add(detailItem);
+        const inventaris = await Inventaris.add(Items);
         res.status(201).json({ code: 201, status: 'created', data: { id: inventaris.id } });
       } catch (error) {
         res.status(500).json({ code: 500, status: 'error', message: error.message });
--- routes/usaha_routes.js
+++ routes/usaha_routes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
 const router = express.Router();
+const multer = require('multer');
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 2 * 1024 * 1024 }, // limit file size to 2MB
+});
 const usaha_controller = require('../controllers/usaha_controller');
 const validator = require('./validator');
 
@@ -9,7 +14,7 @@ const sementara = () => {};
 router.get('/', usaha_controller.getAllUsaha);
 router.get('/:usahaId', usaha_controller.getUsaha);
 router.get('/owner/:userId', usaha_controller.getUsahaByOwner);
-router.post('/', validator.usahaValidationRules(), validator.validate, usaha_controller.addUsaha);
+router.post('/', upload.single('file'), validator.usahaValidationRules(), validator.validate, usaha_controller.addUsaha);
 router.put('/:usahaId', usaha_controller.editUsaha);
 
 module.exports = router;
